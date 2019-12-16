f0f8a2dc8d2921a8c33e95726958868286a13930
wwatson <watson@preferit.com>
2019-12-16 17:17:52 -0600

    Update build-deps.sh

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
 echo "installing go version 1.12.7..."
-apk add --no-cache --virtual .build-deps bash gcc musl-dev openssl go
+apt install -y wget gcc musl-dev openssl
 wget -O go.tgz https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
 tar -C /usr/local -xzf go.tgz
-cd /usr/local/go/src/
-pwd
-ls -al
-./make.bash
-# export PATH="/usr/local/go/bin:$PATH"
-# export GOPATH="/opt/go/"
-# export PATH="$PATH:$GOPATH/bin"
-export PATH=/usr/local/go/bin:$PATH
-export GOPATH=/usr/bin/go
-export PATH=$PATH:$GOPATH/bin
-# apk del .build-deps
-# go version
+export GOPATH=$HOME/work
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
-# curl -O https://dl.google.com/go/go1.11.13.linux-amd64.tar.gz
-#
-# tar -C /usr/local -xzf go1.11.13.linux-amd64.tar.gz 
-
-export PATH=$PATH:/usr/local/go/bin
+go version
